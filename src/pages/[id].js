251c6b5dcd2e3089fd8534c1f4dd284cd2b4c651
Jake Gadaleta <jake.gads@gmail.com>
2025-10-16 14:35:40 -0400

    adds a redirect splash page

---
+++ src/pages/[id].js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
+
+const NumericRedirectPage = ({ params }) => {
+    const { id } = params;
+
+    useEffect(() => {
+        // Check if the id is a number
+        if (id && /^\d+$/.test(id)) {
+            // Redirect to team page
+            navigate(`/team/${id}`, { replace: true });
+        } else {
+            // If not a number, redirect to home page
+            navigate('/', { replace: true });
+        }
+    }, [id]);
+
+    // Return a loading state while redirecting
+    return (
+        <div style={{ 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            height: '100vh',
+            fontSize: '18px'
+        }}>
+            Redirecting...
+        </div>
+    );
+};
+
+export default NumericRedirectPage;
